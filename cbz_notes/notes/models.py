cf3fbbd755e4cfdc9e70ff93b7b8dc850f4c98cb
habelash <habelash@gmail.com>
2020-06-07 19:03:42 +0530

    Chemistry

--- cbz_notes/notes/models.py
+++ cbz_notes/notes/models.py
@@ -8,3 +8,17 @@ class notes(models.Model):
 
     def __str__(self):
         return self.sub_name
+
+    class Meta:
+        verbose_name_plural = "Cbz-Notes"
+
+
+class chemisrty(models.Model):
+    sub_name = models.CharField(max_length=50)
+    files = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.sub_name
+
+    class Meta:
+        verbose_name_plural = "Chemistry"
--- cbz_notes/notes/views.py
+++ cbz_notes/notes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import notes
+from .models import notes, chemisrty
+
 
 def index(request):
-    pdfs = notes.objects.all()
-    return render(request, "index.html", {'pdfs': pdfs})
+    context = {
+        "pdfs": notes.objects.all(),
+        "chem": chemisrty.objects.all()
+    }
+
+    return render(request, "index.html", context)
 # Create your views here.
